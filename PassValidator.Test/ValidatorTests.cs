e9e13b85e195a9b61dc13d41d885957df7daa32d
Tomas McGuinness <tomas@tomasmcguinness.com>
2018-07-25 09:14:02 +0100

    Initial commit

---
+++ PassValidator.Test/ValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PassValidator.Web.Validation;
+
+namespace PassValidator.Test
+{
+    [TestClass]
+    public class ValidatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Validator validator = new Validator();
+            var result = validator.Validate(new byte[0]);
+        }
+    }
+}

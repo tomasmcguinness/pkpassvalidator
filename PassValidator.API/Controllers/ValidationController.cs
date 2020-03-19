0d9b844e0e06a6e1af9089fc4bd62585352ae799
Branimir Malesevic <Branimir.Malesevic@bam.fan>
2020-03-19 15:39:30 +0100

    Add API for pkpass validation

---
+++ PassValidator.API/Controllers/ValidationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassValidator.API.Models;
+using PassValidator.Lib.Validation;
+
+namespace PassValidator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Validate([FromBody] PkPass pkPass)
+        {
+            return Ok(new Validator().Validate(
+                System.Convert.FromBase64String(pkPass.EncodedBytes)));
+        }
+    }
+}
---
+++ PassValidator.API/Models/PkPass.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace PassValidator.API.Models
+{
+    public class PkPass
+    {
+        [JsonProperty("encoded_bytes")]
+        public string EncodedBytes { get; set; }
+    }
+}
